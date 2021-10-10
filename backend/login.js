a69c58b214da4d4b48065acb32ffb1c73a258ae9
Islemmohamed <m.sidelmrabet@esi-sba.dz>
2021-10-10 15:20:08 +0100

    admin page

--- backend/login.js
+++ backend/login.js
@@ -45,7 +45,8 @@ app.post('/auth', function(request, response) {
 //register view
 app.post('/register', function(request, response) {
     
-    var Nom= request.body.Nom;
+	var Nom= request.body.Nom;
+	var image=request.body.image;
     var Prenom= request.body.Prenom;
     var Telephone= request.body.Telephone;
     var Datenaissance= request.body.Datenaissance;
@@ -54,7 +55,7 @@ app.post('/register', function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
-		connection.query('insert into projet2021.user (Nom,Prenom, Telephone, Datenaissance, Email, Role, Username, Password) values (?,?,?,?,?,?,?,? )', [Nom,Prenom, Telephone, Datenaissance, Email, Role, username, password], function(error, results, fields) {
+		connection.query('insert into projet2021.user (Nom,Prenom, Telephone, Datenaissance, Email, Role, Username, Password,image) values (?,?,?,?,?,?,?,?,? )', [Nom,Prenom, Telephone, Datenaissance, Email, Role, username, password,image], function(error, results, fields) {
 			
 
 				response.send(results);
@@ -65,6 +66,47 @@ app.post('/register', function(request, response) {
 		response.send('Please enter Username and Password!');
 		response.end();
 	}
+});
+// all users view
+app.get('/users',function(request,response){
+
+	connection.query('SELECT * FROM user ', function(error, results, fields) {
+		response.send(results);
+					
+		response.end();
+		});
+
+});
+//update user
+app.post('/userUpdate',function(request,response){
+	var idUser=request.body.idUser;
+	var image=request.body.image;
+	var nom= request.body.Nom;
+    var Prenom= request.body.Prenom;
+    var Telephone= request.body.Telephone;
+    var Datenaissance= request.body.Datenaissance;
+    var Email= request.body.Email;
+    var Role= request.body.role;
+	var username = request.body.username;
+	var password = request.body.password;
+	var query='UPDATE user SET Nom="'+nom+'",Prenom="'+Prenom+'", Telephone="'+Telephone+'", Datenaissance="'+Datenaissance+'", Email="'+Email+'", Role="'+Role+'", Username="'+username+'", Password="'+password+'", image="'+image+'" WHERE idUser ='+idUser;
+	connection.query(query, function(error, results, fields) {
+		response.send(query);
+					
+		response.end();
+		});
+		
+
+});
+//delete user
+app.post('/userDelete',function(request,response){
+	var idUser=request.body.idUser;
+	connection.query('Delete FROM user WHERE idUser= ?',[idUser], function(error, results, fields) {
+		response.send(results);
+					
+		response.end();
+		});
+
 });
 
 app.listen(3000);
